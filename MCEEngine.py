3700df1c5967fdeee8078c74cc6a59abfae5e690
micahhurd <micah.hurd@gmail.com>
2020-02-17 14:37:10 -0600

    Added Ask+- command instruction handling

--- MCEEngine.py
+++ MCEEngine.py
@@ -83,28 +83,82 @@ def parseInstruction(instructionLine):
     else:
         return -1
 
+def askCommand(instructionLine):
+    # print(instructionLine)
+
+    # Clean up all the whitespace
+    instructionList = ' '.join(instructionLine.split())
+    # Split the line into a list
+    instructionList = instructionList.split(" ")
+    # print(line)
+
+    # Find the number of elements in the instruction line
+    length = len(instructionList)
+    # A valid command must contain at least three elements
+    if length < 3:
+        return -1
+
+    # Calculate qty of ask parameters
+    qtyParameters = length - 2
+
+    # Determine if the command is setting ask parameters to on or off
+    askFlag = instructionList[1]
+    askFlag = askFlag.lower()
+    if askFlag == "ask+":
+        set = "+"
+    else:
+        set = "-"
+
+    # Loop through the elements in the instruction line list, starting right after the ask element
+    # Sets the ask flag values to on or off as specified
+    for i in range(2,length):
+
+        # Obtain the index'd valued from the instruction list
+        flag = instructionList[i]
+        # set it to lower case character
+        flag = flag.lower()
+        # Index the current flag in the askFlagListID
+        flagIndex = askFlagListID.index(flag)
+        # Set the appropriate flag in the flag values variable to the currect value (on or off)
+        if set == "+":
+            askFlagListValues[flagIndex] = True
+        else:
+            askFlagListValues[flagIndex] = False
+
+    return 1
 
 
 
-procedureLine = 0
 
+
+
+
+
+# Program setup variabls
 cwd = os.getcwd()
 filename = cwd + "/" + "test.mc"
 commandListFile = cwd + "/" + "supportedCommands.txt"
 
 
+# Metcal environment setup variabls
+askFlagListID =     ['a', 'b', 'c', 'd', 'f', 'g', 'j', 'k', 'l', 'm', 'n', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'z']
+askFlagListValues = [True, True, True, True, True, True, True, True, True, True, True, True, True, True, True, True, True, True, True, True, True]
+procedureLine = 0
 instructionList = readMcFile(filename)
 commandList = getCommandList(commandListFile)
 end = False
 
 instructionStartLine = findCommandStart(instructionList) + 1
 
+
+# Main program loop
 procedureLine = instructionStartLine
 while end == False:
     if procedureLine > 100:
         end = True
 
     instructionLine = instructionList[procedureLine]
+    print(instructionLine)
 
     command = parseInstruction(instructionLine)
     if command != -1:
